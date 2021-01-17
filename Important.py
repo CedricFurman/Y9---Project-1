4d31c8293c14c0e5c8022868b1801c4981fdcf12
CedricFurman <72711391+CedricFurman@users.noreply.github.com>
2021-01-17 14:48:19 -0500

    Update Important.py

--- Important.py
+++ Important.py
@@ -1,2 +1,2 @@
 
-# Before running the Music Practice Scheduling Program, make sure to downlaod the Matplotlib module, this can be done by entering "pip3 install matplotlib" in a terminal
+# Before running the Music Practice Scheduling Program, make sure to downlaod Matplotlib, this can be done by entering "pip3 install matplotlib" in a terminal
